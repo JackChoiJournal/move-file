26d5eaea6a05ead8efa005fcfb15eddb241cee17
Jack Choi <jackchoiprogrammer@gmail.com>
2022-08-10 21:07:57 +0800

    feat(Context): Add TasksContext
    TasksContext is used for handler operation related to WatcherHandler's tasks.

---
+++ src/renderer/contexts/TasksContext.tsx
@@ -0,0 +1,43 @@
+import {createContext, useEffect, useState} from "react";
+import {TTask} from "../../main/watcher/types";
+
+interface ITasksContextInterface {
+    tasks: TTask[];
+    currentSource: string;
+    setCurrentSource: (source: string) => void;
+}
+
+type TTasksProviderProps = {
+    children: React.ReactNode;
+}
+
+export const TasksContext = createContext<ITasksContextInterface>({
+    tasks: [],
+    currentSource: "",
+    // @ts-ignore
+    setCurrentSource: (source)=>{}
+});
+
+
+export function TasksProvider({children}: TTasksProviderProps) {
+    let [tasks, setTasks] = useState<TTask[]>([]);
+    let [currentSource, setCurrentSource] = useState<string>("");
+
+    useEffect(() => {
+        window.api.getTasks().then(tasks => {
+            setTasks(tasks);
+        });
+    }, []);
+
+    let tasksContext: ITasksContextInterface = {
+        tasks: tasks,
+        currentSource: currentSource,
+        setCurrentSource: setCurrentSource
+    }
+
+    return (
+        <TasksContext.Provider value={tasksContext}>
+            {children}
+        </TasksContext.Provider>
+    )
+}
