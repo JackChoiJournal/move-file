c4d90c690f2a93193337a535384f81edb0cec806
Jack Choi <jackchoiprogrammer@gmail.com>
2022-08-10 21:01:00 +0800

    refactor(main.ts): Initialize WatcherHandler instance when app is ready

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,6 +14,7 @@ import {autoUpdater} from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import {resolveHtmlPath} from './util';
+import {read, WatcherHandler} from "./watcher/watcher";
 
 declare global {
     var shared: {
@@ -135,7 +136,12 @@ app
             });
         });
     })
-    .then(() => {
+    .then(async () => {
+        let tasks = await read();
+        for (let task of tasks.data) {
+            WatcherHandler.start(task)
+        }
+
         global.shared.watcherHandler = WatcherHandler;
         global.shared.appWindow = mainWindow;
 
