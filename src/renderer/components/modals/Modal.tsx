e5947d812702eaa2defdc40b0f9654750299dba7
Jack Choi <jackchoiprogrammer@gmail.com>
2022-08-17 15:59:38 +0800

    refactor: Change DirectoryListModal component outlook

--- src/renderer/components/modals/Modal.tsx
+++ src/renderer/components/modals/Modal.tsx
@@ -6,7 +6,12 @@ import {useEffect, useState} from "react";
 import {createPortal} from "react-dom";
 import {RenderTree} from "../../../@types/types";
 import {Backdrop} from "./Backdrop";
-import {RefreshButton} from "../Button";
+import {ConfirmButton, RefreshButton} from "../Button";
+import {GrowColumn} from "../Column";
+import {FloatPlaceHolderInput} from "../Input";
+import {Title} from "../Title";
+import {GrowRow} from "../Row";
+import {List} from "../List";
 
 const memoryState: {
     [key: string]: any
@@ -79,6 +84,10 @@ export function DirectoryListModal() {
         ));
     }
 
+    function onConfirm() {
+        // TODO Add new task API here
+        //      Give the API with the source directory, target directory, matched extensions and files.
+    }
 
     function onRefresh() {
         setIsLoaded(false);
@@ -87,29 +96,81 @@ export function DirectoryListModal() {
     let content = isLoaded
         ? (
             <>
-                <TreeView
-                    aria-label="file system navigator"
-                    defaultCollapseIcon={<ExpandMoreIcon/>}
-                    defaultExpandIcon={<ChevronRightIcon/>}
-                    sx={{width: '100%', height: '100%', 'overflowY': 'auto'}}
-                >
-                    {renderTree(directoryTreeCache)}
-                </TreeView>
+                <div className="flex w-full h-full overflow-hidden">
+                    <GrowColumn className="w-1/3">
+                        <div className="text-center font-bold text-md mb-3">
+                            Watch Folder
+                        </div>
+                        <TreeView
+                            aria-label="file system navigator"
+                            defaultCollapseIcon={<ExpandMoreIcon/>}
+                            defaultExpandIcon={<ChevronRightIcon/>}
+                            sx={{
+                                width: '100%',
+                                height: '100%',
+                                'overflowY': 'auto',
+                                'overflowX': 'auto'
+                            }}
+                        >
+                            {renderTree(directoryTreeCache)}
+                        </TreeView>
+                    </GrowColumn>
+                    <GrowColumn className="w-1/3 border-1 border-black">
+                        <div className="text-center font-bold text-md mb-3">
+                            Move To
+                        </div>
+                        <TreeView
+                            aria-label="file system navigator"
+                            defaultCollapseIcon={<ExpandMoreIcon/>}
+                            defaultExpandIcon={<ChevronRightIcon/>}
+                            sx={{
+                                width: '100%',
+                                height: '100%',
+                                'overflowY': 'auto',
+                                'overflowX': 'auto'
+                            }}
+                        >
+                            {renderTree(directoryTreeCache)}
+                        </TreeView>
+                    </GrowColumn>
+                    <GrowColumn className="w-1/3">
+                        <GrowRow className="h-[47.5%] overflow-auto mb-[5%]">
+                            <Title>
+                                File Type
+                                <FloatPlaceHolderInput
+                                    placeholder={"E.g., .mp3;.jpg;.png"}/>
+                            </Title>
+                            <List data={new Set(['.mp3', '.pdf'])}/>
+                        </GrowRow>
+                        <GrowRow className="h-[47.5%] overflow-auto">
+                            <Title>
+                                File Name
+                                <FloatPlaceHolderInput
+                                    placeholder={"E.g., example.pdf;example;txt"}/>
+                            </Title>
+                            <List data={new Set(['example.mp3'])}/>
+                        </GrowRow>
+                    </GrowColumn>
+                </div>
                 <div className="flex justify-around w-full">
-                    <RefreshButton title="Refresh" onClick={onRefresh} />
+                    <ConfirmButton title="Confirm" onClick={onConfirm}/>
+                    <RefreshButton title="Refresh" onClick={onRefresh}/>
                 </div>
             </>
         )
         : <CircularProgress/>
 
     return (
+        // h-[calc(90%+1.25rem)]: 1.25rem is the height of the gap-y-5.
+        // We assure there will be one y gap, so we only need to add one 1.25rem.
+        // Adding 1.25rem to the height to avoid content being overflow.
         <div
-            className="fixed w-[50%] h-[50%]
-            z-20 left-[25%] top-[25%]
+            className="fixed w-[90%] h-[calc(90%+1.25rem)]
+            z-20 left-[5%] top-[calc(5%-1.25rem/2)]
             bg-white shadow-lg shadow-gray-300
             ring-[5px] ring-slate-50/70 rounded-lg
             flex justify-center items-center
-            py-8 px-7 overflow-auto flex flex-col
+            py-8 px-7 flex flex-col
             gap-y-5"
         >
             {content}
